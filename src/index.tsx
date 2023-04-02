c20851bc2c27f98d86e50fb35b3493f8f813aee3
Refat Abdukarimov <usergit@mail.ru>
2023-04-02 14:09:42 +0300

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'components/App'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { store } from 'redux/store'
 import { Provider } from 'react-redux'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
+    <Provider  store={store}>
+      <Router basename='/'>
         <App />
       </Router>
     </Provider>
